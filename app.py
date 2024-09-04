763c2ff06584a42833e74fd97bf7a3412dfea9ec
smarkitan <97811066+smarkitan@users.noreply.github.com>
2024-09-04 10:58:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def get_intraday_stock_data(symbol):
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
